910a834f01369d0cbba9a1c249562cf3919c3cec
NianZo <nicholas.angle@gmail.com>
2021-04-11 15:01:43 -0700

    Adding files generated by CMake when working with the CMake test feature.

---
+++ CMakeFiles/UnitTests.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nic/Documents/Projects/RayTracerChallenge/test/CanvasTest.cpp" "/home/nic/Documents/Projects/RayTracerChallenge/CMakeFiles/UnitTests.dir/CanvasTest.o"
+  "/home/nic/Documents/Projects/RayTracerChallenge/test/ColorTest.cpp" "/home/nic/Documents/Projects/RayTracerChallenge/CMakeFiles/UnitTests.dir/ColorTest.o"
+  "/home/nic/Documents/Projects/RayTracerChallenge/test/MatrixTest.cpp" "/home/nic/Documents/Projects/RayTracerChallenge/CMakeFiles/UnitTests.dir/MatrixTest.o"
+  "/home/nic/Documents/Projects/RayTracerChallenge/test/TransformationTest.cpp" "/home/nic/Documents/Projects/RayTracerChallenge/CMakeFiles/UnitTests.dir/TransformationTest.o"
+  "/home/nic/Documents/Projects/RayTracerChallenge/test/TupleTest.cpp" "/home/nic/Documents/Projects/RayTracerChallenge/CMakeFiles/UnitTests.dir/TupleTest.o"
+  "/home/nic/Documents/Projects/RayTracerChallenge/test/main.cpp" "/home/nic/Documents/Projects/RayTracerChallenge/CMakeFiles/UnitTests.dir/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/UnitTests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnitTests.dir/CanvasTest.o"
+  "CMakeFiles/UnitTests.dir/ColorTest.o"
+  "CMakeFiles/UnitTests.dir/MatrixTest.o"
+  "CMakeFiles/UnitTests.dir/TransformationTest.o"
+  "CMakeFiles/UnitTests.dir/TupleTest.o"
+  "CMakeFiles/UnitTests.dir/main.o"
+  "UnitTests"
+  "UnitTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UnitTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
