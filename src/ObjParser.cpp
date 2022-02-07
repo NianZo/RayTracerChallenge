2ba5d24f02af108a331fa776ed40213dd2cbf9c8
NianZo <nicholas.angle@gmail.com>
2022-02-06 21:07:53 -0800

    Fix vertex parsing logic

--- src/ObjParser.cpp
+++ src/ObjParser.cpp
@@ -8,23 +8,51 @@
 #include "ObjParser.hpp"
 #include <cstdlib>
 #include <charconv>
+#include <array>
 
 ObjParser::ObjParser(const std::string& inputData) noexcept
 {
+	if (inputData.size() == 0)
+	{
+		return;
+	}
+
 	uint64_t oldLinePos = 0;
 	uint64_t newLinePos = inputData.find('\n');
-	while (newLinePos != std::string::npos)
-	{
-		const std::string_view textLine = inputData.substr(oldLinePos, newLinePos - oldLinePos);
-		const uint64_t delimiter1 = textLine.find(' ');
+	newLinePos = newLinePos == std::string::npos ? inputData.size() : newLinePos;
 
-		if (textLine.substr(0, delimiter1 + 1) == "v ")
+	while (oldLinePos <= inputData.size())
+	{
+		std::string_view textLine = std::string_view(inputData).substr(oldLinePos, newLinePos - oldLinePos);
+		//const uint64_t delimiter1 = textLine.find(' ');
+
+		if (textLine.starts_with("v "))
 		{
-			const uint64_t delimiter2 = textLine.find(' ', delimiter1);
-			//const float xVal = stof(textLine, &delimiter1);
-			//const float xVal = std::atof(textLine);//atof(textLine.substr(delimiter1, delimiter2 - delimiter1));
-			float xVal;
-			std::from_chars(textLine.data() + delimiter1, textLine.data() + delimiter2, xVal);
+			uint64_t delimiter = 1; //textLine.remove_prefix(2);
+			std::array<float, 3> vertexPositions;
+			for (uint32_t i = 0; i < 3; i++)
+			{
+				textLine.remove_prefix(delimiter + 1);
+				delimiter = textLine.find(' ');
+				delimiter = delimiter == std::string::npos ? textLine.size() : delimiter;
+				std::from_chars(textLine.data(), textLine.data() + delimiter, vertexPositions[i]);
+
+			}
+//			const uint64_t delimiter2 = textLine.find(' ', delimiter1 + 1);
+//			//const float xVal = stof(textLine, &delimiter1);
+//			//const float xVal = std::atof(textLine);//atof(textLine.substr(delimiter1, delimiter2 - delimiter1));
+//			float xVal;
+//			std::from_chars(textLine.data() + delimiter1 + 1, textLine.data() + delimiter2, xVal);
+//
+//			const uint64_t delimiter3 = textLine.find(' ', delimiter2 + 1);
+//			float yVal;
+//			std::from_chars(textLine.data() + delimiter2 + 1, textLine.data() + delimiter3, yVal);
+//
+//			float zVal;
+//			std::from_chars(textLine.data() + delimiter3 + 1, textLine.data() + textLine.size(), zVal);
+
+			//vertices.emplace_back(Point(xVal, yVal, zVal));
+			vertices.emplace_back(Point(vertexPositions[0], vertexPositions[1], vertexPositions[2]));
 		} else
 		{
 			ignoredLines++;
@@ -32,6 +60,7 @@ ObjParser::ObjParser(const std::string& inputData) noexcept
 
 		oldLinePos = newLinePos + 1;
 		newLinePos = inputData.find('\n', oldLinePos);
+		newLinePos = newLinePos == std::string::npos ? inputData.size() : newLinePos;
 	}
 }
 
