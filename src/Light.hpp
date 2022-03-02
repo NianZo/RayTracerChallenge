ab281a29270f9fde32be0e3bde284d949481e73c
NianZo <nicholas.angle@gmail.com>
2022-03-01 18:33:51 -0800

    Re-format Light::operator== to fix suspected code coverage bug

--- src/Light.hpp
+++ src/Light.hpp
@@ -21,7 +21,10 @@ class Light
     Light() noexcept : position(Point(0, 0, 0)), intensity(Color(1, 1, 1)){};
     Light(const Tuple& positionIn, const Color& intensityIn) noexcept : position(positionIn), intensity(intensityIn){};
 
-    bool operator==(const Light& other) const noexcept { return position == other.position && intensity == other.intensity; };
+    bool operator==(const Light& other) const noexcept
+    {
+    	return position == other.position && intensity == other.intensity;
+    };
 };
 
 #endif /* SRC_LIGHT_HPP_ */
